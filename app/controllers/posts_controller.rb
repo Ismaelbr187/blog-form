2046e7448d3e80f9ad80f3b608d0592ab6bb755f
Ismael <ismael.br187@gmail.com>
2020-08-10 22:50:24 -0400

    validates title, image_url, content

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,5 +8,20 @@ class PostsController < ApplicationController
   end
 
   def create
+
+    temp_content = params[:post][:content].gsub! 'spoiler', ''
+    
+    @post = Post.create(
+      title: params[:post][:title],
+      image_url: params[:post][:image_url],
+      content: temp_content
+    )
+
+    if @post.valid?
+      @post.save
+      redirect_to root_path, notice: 'Post was succesfully created.'
+    else
+      redirect_to root_path, alert: 'Post was not created.'
+    end
   end
 end
